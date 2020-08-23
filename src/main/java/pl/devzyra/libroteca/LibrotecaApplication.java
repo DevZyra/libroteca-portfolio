9ceb5a4f3a156c679ee6de6467a75f3a123d17f5
Michał <devzyra@gmail.com>
2020-08-23 15:08:03 +0200

    initial commit

---
+++ src/main/java/pl/devzyra/libroteca/LibrotecaApplication.java
@@ -0,0 +1,13 @@
+package pl.devzyra.libroteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/devzyra/libroteca/LibrotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.devzyra.libroteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
