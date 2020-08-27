fabbe30f178febe07325034d6c8a8477b0749e6b
Michał <devzyra@gmail.com>
2020-08-27 14:37:44 +0200

    Basic configuration and setting up databases connection. Closes #3

--- libroteca-web/src/main/java/pl/devzyra/LibrotecaApplication.java
+++ libroteca-web/src/main/java/pl/devzyra/LibrotecaApplication.java
@@ -2,8 +2,9 @@ package pl.devzyra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LibrotecaApplication {
 
 	public static void main(String[] args) {
